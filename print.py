b78f560ebc70ac71b76991143e0eef234f9f7184
coders-uzbek <135232410+coders-uzbek@users.noreply.github.com>
2023-06-02 16:46:55 +0300

    Create print.py

---
+++ print.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  2 16:23:09 2023
+
+@author: DELL
+"""
+#print funksyasi ' yoki " bilan boshlanib tugashi kerak
+print("Hayrli tong!") #yoki
+print('Asalomu alaykum')
+#qatorga bo'lishning  usuli \n bor
+print("Salom, \nErtaga maktabga boramiz")
+# o' g' xarflardan quyidagicha foydalanamiz:
+print("Biz o\'rdak va g\'ozlarni ko\'rdik")
+# printda arifmetik amallar ham bajarish mumkin:
+print(5+8*2)
+# // butun, ** darajani, % qoldiqni ifodalaydi:
+print(18//4)
+print(2**4)
+print(8%6) # kabi
+# (#) bu belgi izohni ya'ni commentni bildiradi
