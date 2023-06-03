b77e8581ce0949866da1df325b3d55c4c2a18caa
coders-uzbek <135232410+coders-uzbek@users.noreply.github.com>
2023-06-03 14:48:38 +0300

    Create variable-o'zgaruvchilar.py

---
+++ variable-o'zgaruvchilar.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  2 16:55:54 2023
+
+@author: DELL
+"""
+# o'zgaruvchilar haqida tanishamiz:
+ism = "Quvonchbek"
+yosh = 15
+print(ism)
+print(yosh)
+# o'zgaruvchi nomida 2 va undan ortiq so'z ishtirok etsa (_) bilan ajratiladi:
+ism_sharif = "Quvonchbek Sherboboyev"
+print(ism_sharif)
+# quyidagilarni o'zgaruvchi ya'ni variablae qilib bo'lmaydi:
+#False               class               from                or
+#None                continue            global              pass
+#True                def                 if                  raise
+#as                  elif                in                  try
+#assert              else                is                  while
+#async               except              lambda              with
+#await               finally             nonlocal            yield
+#break               for                 not 
