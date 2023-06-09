d0015929b9fc35ec9cd2c668dba872cc7e3495bb
coders-uzbek <135232410+coders-uzbek@users.noreply.github.com>
2023-06-09 10:20:45 +0300

    Create if-else.py

---
+++ if-else.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  8 09:37:27 2023
+
+@author: DELL
+"""
+avtolar = ['audi','bnw','volvo','tesla']
+for avto in avtolar:
+    if avto == 'bnw':
+        print(avto.upper())
+else:
+        print(avto.title())
+# Audi
+# BNW
+# Volvo
+# Tesla
+# Kichik: a<b
+# Kichik yoki teng: a<=b
+# Katta: a>b
+# Katta yoki teng: a>=b
+# teng emasmi (!=)
+savol = float(input('3*5 nechchi?'))
+if savol != 15:
+    print("Javob xato")
+else:
+    print("Javob to'g'ri")
+# 3*5 nechchi?15
+# Javob to'g'ri
+
+
+
+    
