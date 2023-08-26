5a573d71d35c89b9a84b09c2ab1ef1d52be3e3cd
coders-uzbek <135232410+coders-uzbek@users.noreply.github.com>
2023-08-26 15:14:38 +0300

    Create file 00.py

---
+++ file 00.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 26 14:59:39 2023
+
+@author: Quvonchbek
+"""
+#!!! Quyidagi usul tavsiya qilinmaydi
+file = open("pi.txt")
+PI = file.read()
+print(PI)
+file.close()
